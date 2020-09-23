8359bf2404e4f79fdf7fcb52c13778f84b6d5f6a
Alfs <al02916384@tecmilenio.mx>
2020-09-22 19:01:18 -0500

    Evidnecia

---
+++ src/Sistema.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Sistema {
+    public static BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] arg){
+
+    }
+}
